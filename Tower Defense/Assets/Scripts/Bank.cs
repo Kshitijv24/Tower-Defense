27436627fcbf33d8f2e4bfb686ec3c01c1c38b98
Kshitijv24 <kshitijv24@gmail.com>
2022-10-06 11:45:36 +0530

    added UI Text to show the Gold Points remaining to place a tower

--- Tower Defense/Assets/Scripts/Bank.cs
+++ Tower Defense/Assets/Scripts/Bank.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance;
 
     [SerializeField] int currentBalance;
+
+    [SerializeField] TextMeshProUGUI displayBalance;
     public int CurrentBalance 
     {
         get
@@ -17,21 +20,29 @@ public class Bank : MonoBehaviour
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     private void ReloadScene()
